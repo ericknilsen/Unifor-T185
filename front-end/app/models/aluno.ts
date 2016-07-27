298fd182217090d9d6cdc98b995be19fb00f53bb
Erick Souza <ericknilsen@gmail.com>
2016-07-27 18:00:25 -0300

    first commit

--- front-end/app/models/aluno.ts
+++
@@ -1,8 +0,0 @@
-export class Aluno {
-	constructor(
-		public id?: number,
-		public nome?: string,
-		public email?: string,
-		public idade?: number,
-		public curso?:string){}
-}
--- front-end/app/models/curso.ts
+++
@@ -1,5 +0,0 @@
-export class Curso {
-	constructor(
-		public codigo: string,
-		public nome: string){}
-}
